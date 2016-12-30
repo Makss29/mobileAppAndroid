0bccb0f078fcb4029cf89da43962bfc04f435075
mae <mae@itrezzo.com>
2016-12-30 15:12:05 +0200

    Test

---
+++ App4.Droid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Azure.Mobile;
+
+namespace App4.Droid
+{
+	[Activity (Label = "App4", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new App4.App ());
+
+            MobileCenter.Configure("972ddf0e-4c38-4db2-982e-190b18606c0a");
+        }
+	}
+}
+
